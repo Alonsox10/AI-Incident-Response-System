a40effd377a717e3b5a16697b11731b40f195dd8
Alonso Navas <miguel.alonso.navas.leon.ravinez@linkhub.ia>
2026-05-11 17:53:17 -0500

    Conexion a  la base de datos

---
+++ database/db.py
@@ -0,0 +1,31 @@
+import asyncpg
+from dotenv import load_dotenv
+import os
+from loguru import logger
+import asyncio
+
+
+
+load_dotenv(override=True)
+
+DATABASE_URL = os.getenv("POSTGRES_URL")
+if not DATABASE_URL:
+    raise ValueError("La URL de la base de datos no está configurada en las variables de entorno.")
+
+async def get_connection():
+    try:
+
+        connection = await asyncpg.connect(DATABASE_URL)
+        logger.info("Conexión a la base de datos establecida exitosamente.")
+        return connection
+    
+    except Exception as e:
+        logger.error(f"Error al conectar a la base de datos: {e}")
+        raise 
+
+
+
+
+asyncio.run(get_connection())
+
+    
