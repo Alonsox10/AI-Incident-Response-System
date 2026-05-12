62203548a4d30db8459f1fdacb103791e356834c
Alonso Navas <miguel.alonso.navas.leon.ravinez@linkhub.ia>
2026-05-11 19:10:18 -0500

    grafo inicial

--- graphs/incidente_graph.py
+++ graphs/incidente_graph.py
@@ -6,8 +6,8 @@ from agents.classifier_agent import classify_agent
 # Define los nodos del grafo y sus transiciones
 graph = StateGraph(IncidentState)
 graph.add_node("classifier", classify_agent)
-graph.set_entry_point("classifier")
-graph.edges("classifier",END)
+graph.set_entry_point("classifier") # El nodo de entrada es el clasificador
+graph.add_edge("classifier", END)
 
 
 app = graph.compile()
