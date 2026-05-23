3e7980658df5252b45c5413e4ee3d21c87c0ab6b
Alonso Navas <miguel.alonso.navas.leon.ravinez@linkhub.ia>
2026-05-22 19:08:35 -0500

    Main actualizado con agente orquestador

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Response , Request
 from graphs.incidente_graph import app as incident_app
 from pydantic import BaseModel
 from langchain_core.messages import HumanMessage
 from loguru import logger
-from service.conversation_service import create_conversation, get_latest_conversation
-
+import uuid
 app = FastAPI()
 
 
@@ -13,24 +12,42 @@ class IncidentRequest(BaseModel):
 
 
 @app.post("/incident")
-def handle_incident(request: IncidentRequest):
+def handle_incident(request: IncidentRequest, requests: Request , response: Response):
     try:
 
-        # Crea una nueva conversación y obtiene su ID
-        conversation_id = get_latest_conversation()
-        if not conversation_id:
-            conversation_id = create_conversation()
+        # Busca la nueva seccion en la cooki
+        session_id = requests.cookies.get("session_id")
+
+        if not session_id:
+            session_id = str(uuid.uuid4())
+
+            response.set_cookie(key="session_id", value=session_id)
 
         # Prepara el estado inicial con el mensaje del usuario
         initial_state = {
             "messages": [
                 HumanMessage(content=request.user_input)
-            ]
+                
+            ],
+            "category": None,
+            "priority": None,
+            "possible_causes": None,
+            "recommendations": None,
+            "current_step": None,
         }
-        config = {"configurable":{"thread_id": conversation_id}}
+        config = {"configurable":{"thread_id": session_id}}
         result =  incident_app.invoke(initial_state, config=config)
-        return {
-            "response": result["messages"][-1].content
+        
+        if not result:
+            return {
+                "error": "No se pudo procesar el incidente"
+            }
+        return{
+            "category": result.get("category"),
+            "priority": result.get("priority"),
+            "possible_causes": result.get("possible_causes"),
+            "recommendations": result.get("recommendations"),
+            "current_step": result.get("current_step")
         }
     except Exception as e:
         logger.error(f"Hubo un error: {e}")
