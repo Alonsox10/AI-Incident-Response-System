4f64e226e485b50659bb8d865a587cfb0dd23198
Alonso Navas <miguel.alonso.navas.leon.ravinez@linkhub.ia>
2026-05-22 19:12:18 -0500

    Cambios adicionales

--- schemas/models.py
+++ schemas/models.py
@@ -1,9 +1,22 @@
 from pydantic import BaseModel
-from typing import List
+from typing import List, Literal
 
 
-class IncidentClassification(BaseModel):
+class IncidentClassificationOutput(BaseModel):
     category: str
     priority: str
     possible_causes: List[str]
+
+
+
+class RecommendationOutput(BaseModel):
+
+    possible_causes: List[str]
     recommendations: List[str]
+
+
+class OrquestatorOutput(BaseModel):
+    next_agent: Literal["classifier", "recommendation", "end"]
+
+
+
--- tools/incidente_tools.py
+++ tools/incidente_tools.py
@@ -32,8 +32,10 @@ def search_incident_by_category(category: str):
 
 @tool
 def insert_incident(title: str, category: str, priority: str):
-    """Inserta un nuevo incidente en la base de datos con título, categoría y prioridad."""
+    """Inserta un nuevo incidente en la base de datos."""
+
     connection = None
+
     try:
         connection = get_connection()
 
@@ -43,12 +45,22 @@ def insert_incident(title: str, category: str, priority: str):
         """
 
         connection.execute(query, (title, category, priority))
+
         connection.commit()
-    
+
+        return {
+            "status": "success",
+            "message": "Incidente registrado correctamente"
+        }
+
     except Exception as e:
         logger.error(f"Error al insertar incidente: {e}")
-        raise ValueError("Ocurrió un error al insertar el incidente.")
-    
+
+        return {
+            "status": "error",
+            "message": str(e)
+        }
+
     finally:
         if connection:
             connection.close()
