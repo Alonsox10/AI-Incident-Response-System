25ed6e5d80b2014f1eb2394a1ed540633b301397
Miguel Alonso Navas Leon Ravinez <leonravinezalonso@gmail.com>
2026-06-21 14:02:35 -0500

    Modelo del llm cambiado

--- models/llm.py
+++ models/llm.py
@@ -10,6 +10,6 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 if not OPENAI_API_KEY:
     raise ValueError("La clave de API de OpenAI no está configurada en las variables de entorno.")
 
-llm = ChatOpenAI(model="gpt-4", temperature=0)
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
 
 llm_bind_tools = llm.bind_tools([search_incident_by_category, insert_incident])
